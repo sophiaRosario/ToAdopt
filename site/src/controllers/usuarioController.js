bdf85f1ca311c1b8495eb3be064ce2cfe7ea1c69
sophiaRosario <sophia.jesus@sptech.school>
2022-11-26 20:24:48 -0300

    Adicionando Quiz já conectado na api e funcionando

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -100,9 +100,39 @@ function cadastrar(req, res) {
     }
 }
 
+function pontoRegistra(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var pontos = req.body.pontosServer;
+    var fkUsuario = req.body.fkUsuarioServer;
+    
+
+    // Faça as validações dos valores
+    if (pontos == undefined) {
+        res.status(400).send("Seu pontos estão undefined!");
+    } else {
+        
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.pontoRegistra(pontos, fkUsuario)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
 module.exports = {
     entrar,
     cadastrar,
     listar,
-    testar
+    testar,
+    pontoRegistra
 }
--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -30,9 +30,24 @@ function cadastrar(nome, sobrenome, email, telefone, senha) {
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
+//quiz
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
+function pontoRegistra(pontos, fkUsuario) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function pontoRegistra():");
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucao = `
+        INSERT INTO respostaQuiz (pontos, fkUsuario) VALUES (${pontos}, ${fkUsuario});
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
 
 module.exports = {
     entrar,
     cadastrar,
     listar,
+    pontoRegistra
 };
